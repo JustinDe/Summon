be08d55a14548093075ed2fe5ae3baa12bfeb1c2
Justin Demetroff <demetroffj@gmail.com>
2018-11-23 07:57:30 -0500

    Added deadend halls prefab and made it so buildgridd adds said prefabs

--- Assets/Scripts/BuildGrid.cs
+++ Assets/Scripts/BuildGrid.cs
@@ -6,17 +6,20 @@ public class BuildGrid : MonoBehaviour {
 
     public GameObject RoomPrefab;
     public GameObject DoorPrefab;
-    public bool setDoors;
+    public GameObject DeadendHallPrefab;
+    public bool setDoors = true;
+    public bool setHalls = true;
     private int gridRoomX = 5;
-    private int gridRoomY = 5;
+    private int gridRoomZ = 5;
     private float roomSpacing = 10f;
     private float doorOffset = 3f;
+    private float hallLengthHalf = 3f;
 
     void Start()
     {
         //Spawn the rooms
         int count = 0;
-        for (int y = 0; y < gridRoomY; y++)
+        for (int y = 0; y < gridRoomZ; y++)
         {
             for (int x = 0; x < gridRoomX; x++)
             {
@@ -31,7 +34,7 @@ public class BuildGrid : MonoBehaviour {
         if (setDoors == true)
         {
             // Spawn the doors that go North-South
-            for (int y = 0; y < gridRoomY; y++)
+            for (int y = 0; y < gridRoomZ; y++)
             {
                 for (int x = 0; x < gridRoomX + 1; x++)
                 {
@@ -50,7 +53,7 @@ public class BuildGrid : MonoBehaviour {
             }
 
             //Spawn the doors that go East-West
-            for (int y = 0; y < gridRoomY + 1; y++)
+            for (int y = 0; y < gridRoomZ + 1; y++)
             {
                 for (int x = 0; x < gridRoomX; x++)
                 {
@@ -58,7 +61,7 @@ public class BuildGrid : MonoBehaviour {
                     GameObject doorEW = Instantiate(DoorPrefab, posEW, Quaternion.Euler(0, 90, 0));
                     doorEW.name = string.Format("door:ew:{0}:{1}", x, y);
                     doorEW.tag = "DoorI";
-                    if (y == 0 || y == gridRoomY)
+                    if (y == 0 || y == gridRoomZ)
                     {
                         doorEW.tag = "DoorN";
                     }
@@ -67,6 +70,25 @@ public class BuildGrid : MonoBehaviour {
                         doorEW.tag = "DoorI";
                     }
                 }
+            }
+        }
+        if (setHalls == true)
+        {
+            //Spawn the hallways
+            for (int i = 0; i < gridRoomX; i++)
+            {
+                Vector3 posN = new Vector3((i * roomSpacing) + doorOffset, 0, 1 - hallLengthHalf);
+                Vector3 posS = new Vector3((i * roomSpacing) + doorOffset + 2, 0, (1 - hallLengthHalf) + (gridRoomX * roomSpacing));
+                GameObject doorN = Instantiate(DeadendHallPrefab, posN, Quaternion.Euler(0, 0, 0));
+                GameObject doorS = Instantiate(DeadendHallPrefab, posS, Quaternion.Euler(0, 180, 0));
+
+            }
+            for (int i = 0; i < gridRoomZ; i++)
+            {
+                Vector3 posE = new Vector3(2 - hallLengthHalf, 0, (i * roomSpacing) + (doorOffset + 1));
+                Vector3 posW = new Vector3((2 - hallLengthHalf) + (gridRoomZ * roomSpacing), 0, (i * roomSpacing) + (doorOffset - 1));
+                GameObject doorE = Instantiate(DeadendHallPrefab, posE, Quaternion.Euler(0, 90, 0));
+                GameObject doorW = Instantiate(DeadendHallPrefab, posW, Quaternion.Euler(0, 270, 0));
             }
         }
     }
